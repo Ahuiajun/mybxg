eb0726f13385411ab7e733453b8aa38ee5766659
code1031 <code1031247267@qq.com>
2017-09-25 09:45:15 +0800

    添加表单验证和提交插件

--- public/js/config.js
+++ public/js/config.js
@@ -5,6 +5,10 @@ require.config({
 		cookie:'jquery-cookie/jquery.cookie',
 		template:'artTemplate/template-web',
 		bootstrap:'bootstrap/js/bootstrap.min',
+		datepicker:'bootstrap-datepicker/js/bootstrap-datepicker',
+		language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+		validate:'validate/jquery-validate',
+		form:'jquery-form/jquery.form',
 		until:'../js/until',
 		common:'../js/common',
 		login:'../js/login',
@@ -13,6 +17,12 @@ require.config({
 	},
 	shim:{
 		bootstrap:{
+			deps:['jquery']
+		},
+		language:{
+			deps:['jquery','datepicker']
+		},
+		validate:{
 			deps:['jquery']
 		}
 	}
--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','until'],function($,template,until){
+define(['jquery','template','until','datepicker','language','validate','form'],function($,template,until){
 	var tcId = until.qs('tc_id');
 	if(tcId){
 		//编辑操作
@@ -24,21 +24,52 @@ define(['jquery','template','until'],function($,template,until){
 	}
 	// 实现表单提交
 	function submitForm(url){
-		$('#teacherBtn').click(function(){
-			$.ajax({
-				type:'post',
-				url:url,
-				data:$('#teacherForm').serialize(),
-				dataType:'json',
-				success:function(data){
-					if(data.code==200){
-						location.href='/teacher/list';
+		$('#teacherForm').validate({
+			sendForm:false,
+			valid:function(){
+				console.log("success")
+				$(this).ajaxSubmit({
+					type:'post',
+					url:url,
+					dataType:'json',
+					success:function(data){
+						if(data.code==200){
+							location.href = '/teacher/list';
+						}
 					}
-				}
-
-			})
+				});
+			},
+			 description : {
+                tcName : {
+                    required : '用户名不能为空'
+                },
+                tcPass : {
+                    required : '密码不能为空',
+                    pattern : '密码只能是6位数字'
+                },
+                tcJoindate : {
+                    required : '入职时间不能为空'
+                }
+            }
 		})
 	}
+
+	// function submitForm(url){
+	// 	$('#teacherBtn').click(function(){
+	// 		$.ajax({
+	// 			type:'post',
+	// 			url:url,
+	// 			data:$('#teacherForm').serialize(),
+	// 			dataType:'json',
+	// 			success:function(data){
+	// 				if(data.code==200){
+	// 					location.href='/teacher/list';
+	// 				}
+	// 			}
+
+	// 		})
+	// 	})
+	// }
 
 
 
